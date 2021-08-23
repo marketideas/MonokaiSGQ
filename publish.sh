e92ef04054a11075ea49c141f3c90dd40f07f91e
Shawn Quick <quick@marketideas.com>
2021-08-23 14:04:20 -0700

    first publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source ~/keys/aws_squick.txt
 
-echo $VSCODE_PUBLISH_TOKEN >> vsce login marketideas
+vsce logout marketideas
+echo $VSCODE_PUBLISH_TOKEN |  vsce login marketideas
+vsce package
+vsce publish
+
