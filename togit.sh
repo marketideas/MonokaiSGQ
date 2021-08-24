c7367e28f5a98a254a0e108c40449145fdccfc33
Shawn Quick <quick@marketideas.com>
2021-08-24 11:25:33 -0700

    automated push from svn

--- togit.sh
+++ togit.sh
@@ -7,9 +7,14 @@ rm -rf $DIR
 mkdir -p $DIR
 cd $DIR
 git clone git@github.com:/marketideas/$REPO
-cp -R $P $DIR
+
+#cp -R $P $DIR
 cd $DIR/$REPO
-#git status
+git checkout sgq_work_branch
+cp -R $P $DIR
+rm -rf .svn
+git status
+
 git add *
 git status
 git commit -m "automated push from svn"
